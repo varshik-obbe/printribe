c2b044c9284e6b6be38fe000fb7aa78c82003df3
Dhananjay Soni <arcticdevss@gmail.com>
2022-01-12 19:18:06 +0530

    nav and footer links

--- src/components/catalog.jsx
+++ src/components/catalog.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import img from '../assets/mensclothing.PNG'
 import img1 from '../assets/womensclothing.PNG'
 import styles from '../styles/catalog.module.css'
+
 function Catalog() {
     return (
         <>
@@ -9,34 +10,44 @@ function Catalog() {
                 <div className="row mt-3 mb-3 mx-0">
                     <div className="col-lg-4 col-md-6 col-sm-12 p-2">
                         <div className={`card ${styles.catalogcontainer}`} >
-                            <img class="card-img-top" src={img} alt="mensclothing"/>
-                            <div class ="card-body">
-                            <p class ="card-text fw-bold">Men's Clothing</p>
-                            </div>
+                            <a href="/product-page" className={styles.catalogText}>
+                                <img class="card-img-top" src={img} alt="mensclothing" />
+                                <div class="card-body">
+                                    <p class="card-text fw-bold">Men's Clothing</p>
+                                </div></a>
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-6 col-sm-12 p-2">
                         <div className={`card ${styles.catalogcontainer}`}>
-                            <img class="card-img-top" src={img1} alt="womensclothing"/>
-                            <div class ="card-body">
-                            <p class ="card-text fw-bold">Women's Clothing</p>
-                            </div>
+                            <a href="/product-page" className={styles.catalogText}>
+
+                                <img class="card-img-top" src={img1} alt="womensclothing" />
+                                <div class="card-body">
+                                    <p class="card-text fw-bold">Women's Clothing</p>
+                                </div>
+                            </a>
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-6 col-sm-12 p-2">
                         <div className={`card ${styles.catalogcontainer}`}>
-                            <img class="card-img-top" src={img} alt="mensclothing"/>
-                            <div class ="card-body">
-                            <p class ="card-text fw-bold">Men's Clothing</p>
+                        <a href="/product-page" className={styles.catalogText}>
+
+                            <img class="card-img-top" src={img} alt="mensclothing" />
+                            <div class="card-body">
+                                <p class="card-text fw-bold">Accessories</p>
                             </div>
+                            </a>
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-6 col-sm-12 p-2">
                         <div className={`card ${styles.catalogcontainer}`}>
-                            <img class="card-img-top" src={img1} alt="womensclothing"/>
-                            <div class ="card-body">
-                            <p class ="card-text fw-bold">Women's Clothing</p>
+                        <a href="/product-page" className={styles.catalogText}>
+
+                            <img class="card-img-top" src={img1} alt="womensclothing" />
+                            <div class="card-body">
+                                <p class="card-text fw-bold">Home & Living</p>
                             </div>
+                            </a>
                         </div>
                     </div>
 
