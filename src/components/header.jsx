34f7203f15767f1b36617833ff2a11fbc0e2f2e3
varshik60 <=>
2022-01-12 17:02:25 +0530

    sign up,sign in and account page functionality implemented

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,38 +1,59 @@
+import {Nav, Navbar} from "react-bootstrap";
+
 import React from "react";
-import { Nav, Navbar } from "react-bootstrap";
 import logo from "../assets/Printribe-logo.png";
 
 function Header() {
-  return (
-    <>
-      <div className="container-fluid p-3">
-        <div className="row">
-          <div className=" col-lg-4 col-12 logo text-center mb-3 mb-sm-0 py-2">
-            <img
-              src={logo}
-              alt=""
-              style={{ height: "35px", maxWidth: "100%" }}
-            />
-          </div>
-          <div className="col-lg-5 col-8 px-3 py-2">
-            <input className="form-control" placeholder="search"></input>
-          </div>
-          <div className="col-lg-3 col-4">
-            <Navbar expand="lg" className="">
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mx-auto justify-content-evenly w-100">
-                  <Nav.Link href="/signup">Sign up</Nav.Link>
-                  <Nav.Link href="/signin">Sign In</Nav.Link>
-                  <Nav.Link href="#">My Cart</Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Navbar>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="container-fluid p-3">
+				<div className="row">
+					<div className=" col-lg-4 col-12 logo text-center mb-3 mb-sm-0 py-2">
+						<a href="/">
+							<img
+								src={logo}
+								alt=""
+								style={{height: "35px", maxWidth: "100%"}}
+							/>
+						</a>
+					</div>
+					<div className="col-lg-5 col-8 px-3 py-2">
+						<input className="form-control" placeholder="search"></input>
+					</div>
+					<div className="col-lg-3 col-4">
+						<Navbar expand="lg" className="">
+							<Navbar.Toggle aria-controls="basic-navbar-nav" />
+							<Navbar.Collapse id="basic-navbar-nav">
+								<Nav className="mx-auto justify-content-evenly w-100">
+									{localStorage.getItem("customerId") ? (
+										<>
+											<Nav.Link href="#">My Cart</Nav.Link>
+											<Nav.Link href="/my-account">My Account</Nav.Link>
+											<Nav.Link
+												href="/"
+												onClick={() => {
+													localStorage.removeItem("token");
+													localStorage.removeItem("customerId");
+												}}
+											>
+												Logout
+											</Nav.Link>
+										</>
+									) : (
+										<>
+											<Nav.Link href="#">My Cart</Nav.Link>
+											<Nav.Link href="/signup">Sign up</Nav.Link>
+											<Nav.Link href="/signin">Sign In</Nav.Link>
+										</>
+									)}
+								</Nav>
+							</Navbar.Collapse>
+						</Navbar>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Header;
--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,184 +1,230 @@
+import axios from "axios";
 import React from "react";
-
 // ---- import Google Icon from ReactIcons ---- //
-import {FcGoogle} from "react-icons/all";
-
+import { FcGoogle } from "react-icons/all";
+import { Link, useNavigate } from "react-router-dom";
+import Api from "../api/api";
+import ClothingRock from "../assets/Clothing Rack.png";
 // ---- import PNG from Assets ---- //
 import PrintribeLogo from "../assets/Printribe-logo.png";
-import ClothingRock from "../assets/Clothing Rack.png";
-
 // ---- import Styles ---- //
 import classes from "../styles/signinsignup.module.css";
 
 function Signin() {
-	const handleGoogleSignUp = () => {
-		// Google SignUp Event
-		alert("Google SignUp");
-	};
+  let navigate = useNavigate();
 
-	const handleEmailSignUp = () => {
-		// Email Sign Up Event
-		alert("Email Sign Up");
-	};
+  const [state, setState] = React.useState({
+    email: "",
+    password: "",
+    error: false,
+  });
 
-	const handleChangeEmail = () => {
-		// Change Email Event
-	};
+  const handleGoogleSignUp = () => {
+    // Google SignUp Event
+    alert("Google SignUp");
+  };
 
-	const handleChangePassword = () => {
-		// Change Password Event
-	};
+  const handleEmailSignUp = () => {
+    // Email Sign Up Event
+    alert("Email Sign Up");
+  };
 
-	const handleSignIn = () => {
-		// SignIn Button Press
-		alert("Sign in");
-	};
+  const handleChangeEmail = (event) => {
+    // handle Input Email //
+    setState((prev) => ({ ...prev, email: event.target.value }));
+  };
 
-	const handleForgotPassword = () => {
-		// ForgotPassword
-		alert("Forgot Password");
-	};
+  const handleChangePassword = (event) => {
+    // handle Input Password //
+    setState((prev) => ({ ...prev, password: event.target.value }));
+  };
 
-	const handleGoogleSignIn = () => {
-		// GoogleSignIn
-		alert("Google SignIn");
-	};
+  const handleSignIn = () => {
+    // User Login Api Call //
+    axios
+      .post(Api.customers.LOGIN, {
+        credentials: {
+          password: state.password,
+          email: state.email,
+          role: "customer",
+        },
+      })
+      .then((res) => {
+        console.log("login success", res);
+        localStorage.setItem(
+          "token",
+          res.data.customer.token ? res.data.customer.token : ""
+        );
+        localStorage.setItem(
+          "customerId",
+          res.data.customer.id ? res.data.customer.id : ""
+        );
+        setState((prev) => ({ ...prev, error: false }));
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        setState((prev) => ({ ...prev, error: true }));
+      });
+  };
 
-	return (
-		// ---- Main Container ---- //
-		<div className="container-fluid" style={{minHeight: "500px"}}>
-			<div className="row">
-				<div className="col-12 px-5 py-5">
-					<img src={PrintribeLogo} alt="" style={{height: "45px"}} />
-				</div>
-			</div>
-			<div className="container">
-				<div className="row">
-					<div className="col-12 col-sm-12 col-md-6 col-lg-6">
-						<div
-							// className="d-flex flex-column align-items-center"
-							className={classes.divForm}
-						>
-							<h2 class="fw-bold">Sign up to Printribe</h2>
-							{/* ----- Google Sign Up Button ----- */}
-							<div>
-								<button
-									type="button"
-									class="btn btn-outline-secondary w-100"
-									style={{color: "#000", marginTop: "10px"}}
-									onClick={handleGoogleSignUp} // Sign Up with Google Click Event //
-								>
-									<span>
-										<FcGoogle style={{fontSize: "25px"}} /> Sign up with Google
-									</span>
-								</button>
-							</div>
-							{/* ----- Sign Up with Email Button ----- */}
-							<div>
-								<button
-									type="button"
-									class="btn btn-primary w-100 fw-bold"
-									style={{
-										color: "#FFF",
-										background: "#1F649F",
-										marginTop: "15px",
-									}}
-									onClick={handleEmailSignUp} // Sign Up with Email Click Event //
-								>
-									Sign up with your email
-								</button>
-							</div>
-							<br />
-							<div class="d-flex">
-								<span>Already a member?</span>
-								<span class="mx-4">Sign in now</span>
-							</div>
-							<br />
-							{/* ----- Email Input ----- */}
-							<div>
-								<label for="basic-url" class="form-label mb-0">
-									Email
-								</label>
-								<div class="input-group mb-2">
-									<input
-										type="text"
-										class="form-control"
-										id="basic-url"
-										aria-describedby="basic-addon3"
-										onChange={handleChangeEmail} // Email Change Event //
-									/>
-								</div>
-							</div>
-							{/* ----- Password Input ----- */}
-							<div>
-								<label for="basic-url" class="form-label mb-0">
-									Password
-								</label>
-								<div class="input-group mb-2">
-									<input
-										type="text"
-										class="form-control"
-										id="basic-url"
-										aria-describedby="basic-addon3"
-										onChange={handleChangePassword} // Password Change Event //
-									/>
-								</div>
-							</div>
-							<div class="d-flex justify-content-between">
-								{/* ----- SignIn Button ----- */}
-								<button
-									type="button"
-									class="btn w-25"
-									style={{background: "#EE3C2F", color: "#FFF"}}
-									onClick={handleSignIn} // Sign In Click Event //
-								>
-									Sign in
-								</button>
+  const handleForgotPassword = () => {
+    // ForgotPassword
+    alert("Forgot Password");
+  };
 
-								{/* ----- Forgot Password Button ----- */}
-								<button
-									type="button"
-									style={{
-										color: "#000",
-										border: "0",
-										background: "transparent",
-									}}
-									onClick={handleForgotPassword} // Forgot Password Click Event //
-								>
-									Forgot Password?
-								</button>
-							</div>
-							<div class="d-flex align-items-center">
-								<hr class="w-100" />
-								<span class="mx-3">or</span>
-								<hr class="w-100" />
-							</div>
+  const handleGoogleSignIn = () => {
+    // GoogleSignIn
+    alert("Google SignIn");
+  };
 
-							{/* ----- Sign in with Google Button ----- */}
-							<button
-								type="button"
-								class="btn btn-outline-secondary w-100"
-								style={{color: "#000"}}
-								onClick={handleGoogleSignIn}
-							>
-								<span>
-									<FcGoogle style={{fontSize: "25px"}} /> Sign in with Google
-								</span>
-							</button>
-						</div>
-					</div>
-					<div className="col-12 col-sm-12 col-md-6 col-lg-6">
-						<img
-							alt=""
-							class="my-5"
-							src={ClothingRock}
-							style={{height: "100%", width: "100%", objectFit: "cover"}}
-						/>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    // ---- Main Container ---- //
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12 px-5 pt-5 pb-4">
+          <img src={PrintribeLogo} alt="" style={{ height: "40px" }} />
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+            <div
+              // className="d-flex flex-column align-items-center"
+              className={classes.divForm}
+            >
+              <h2 class="fw-bold">Sign up to Printribe</h2>
+              {/* ----- Google Sign Up Button ----- */}
+              <div>
+                <button
+                  type="button"
+                  class="btn btn-outline-secondary w-100"
+                  style={{ color: "#000", marginTop: "10px" }}
+                  onClick={handleGoogleSignUp} // Sign Up with Google Click Event //
+                  disabled
+                >
+                  <span>
+                    <FcGoogle style={{ fontSize: "25px" }} /> Sign up with
+                    Google
+                  </span>
+                </button>
+              </div>
+              {/* ----- Sign Up with Email Button ----- */}
+              <div>
+                <Link to="/signup">
+                  <button
+                    type="button"
+                    class="btn btn-primary w-100 fw-bold"
+                    style={{
+                      color: "#FFF",
+                      background: "#1F649F",
+                      marginTop: "15px",
+                    }}
+                  >
+                    Sign up with your email
+                  </button>
+                </Link>
+              </div>
+              <div class="d-flex mt-3">
+                <span>Already a member?</span>
+                <span class="mx-4">Sign in now</span>
+              </div>
+              {/* ----- Email Input ----- */}
+              <div>
+                <label for="basic-url" class="form-label mt-2">
+                  Email
+                </label>
+                <div class="input-group mb-2">
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="basic-url"
+                    aria-describedby="basic-addon3"
+                    onChange={handleChangeEmail} // Email Change Event //
+                  />
+                </div>
+              </div>
+              {/* ----- Password Input ----- */}
+              <div>
+                <label for="basic-url" class="form-label mb-0">
+                  Password
+                </label>
+                <div class="input-group mb-2">
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="basic-url"
+                    aria-describedby="basic-addon3"
+                    onChange={handleChangePassword} // Password Change Event //
+                  />
+                </div>
+              </div>
+              {state.error && (
+                <div>
+                  <label class="mb-2 text-danger">
+                    Please Enter Correct Email or Password
+                  </label>
+                </div>
+              )}
+              <div class="d-flex justify-content-between">
+                {/* ----- SignIn Button ----- */}
+                <button
+                  type="button"
+                  class="btn w-25"
+                  disabled={
+                    state.email === "" || state.password === "" ? true : false
+                  }
+                  style={{ background: "#EE3C2F", color: "#FFF" }}
+                  onClick={handleSignIn} // Sign In Click Event //
+                >
+                  Sign in
+                </button>
+
+                {/* ----- Forgot Password Button ----- */}
+                <button
+                  type="button"
+                  style={{
+                    color: "#000",
+                    border: "0",
+                    background: "transparent",
+                  }}
+                  onClick={handleForgotPassword} // Forgot Password Click Event //
+                >
+                  Forgot Password?
+                </button>
+              </div>
+              <div class="d-flex align-items-center">
+                <hr class="w-100" />
+                <span class="mx-3">or</span>
+                <hr class="w-100" />
+              </div>
+
+              {/* ----- Sign in with Google Button ----- */}
+              <button
+                type="button"
+                class="btn btn-outline-secondary w-100"
+                style={{ color: "#000" }}
+                onClick={handleGoogleSignIn}
+                disabled
+              >
+                <span>
+                  <FcGoogle style={{ fontSize: "25px" }} /> Sign in with Google
+                </span>
+              </button>
+            </div>
+          </div>
+          <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+            <img
+              alt=""
+              class="my-5"
+              src={ClothingRock}
+              style={{ height: "100%", width: "100%", objectFit: "cover" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Signin;
