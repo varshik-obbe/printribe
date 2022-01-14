48331e8c8551d6b5b7dc0e3a18f5fd3a02889413
Prasath30 <prasathbalasubramani@gmail.com>
2022-01-14 18:30:13 +0530

    added-colors

--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
-=======
 import axios from 'axios';
 import "dotenv";
 require('dotenv').config();
 
 
->>>>>>> 9c57f01c244f652c4436fa401a50aaaff8fbe945
 
 axios.defaults.baseURL = 'https://api.theprintribe.com/api';
 ReactDOM.render(
